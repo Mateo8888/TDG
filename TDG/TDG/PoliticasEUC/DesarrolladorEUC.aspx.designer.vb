ab8fd5bc9cd15d958888593bcc9815716a53afaa
Mateo8888 <80359077+Mateo8888@users.noreply.github.com>
2025-10-12 17:28:19 -0500

    Validaciones en el front
    Validaciones, mensajes y ajustes en formularios del front
    
    Sigo con error al ejecutar desarrollador.aspx (no se encontró sub main)

--- TDG/TDG/PoliticasEUC/DesarrolladorEUC.aspx.designer.vb
+++ TDG/TDG/PoliticasEUC/DesarrolladorEUC.aspx.designer.vb
@@ -1,159 +1,50 @@
-﻿'------------------------------------------------------------------------------
-' <generado automáticamente>
-'     Este código fue generado por una herramienta.
-'
-'     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
-'     se vuelve a generar el código. 
-' </generado automáticamente>
-'------------------------------------------------------------------------------
-
-Option Strict On
+﻿Option Strict On
 Option Explicit On
 
+Namespace TDG
+    Partial Public Class DesarrolladorEUC
 
-Partial Public Class DesarrolladorEUC
+        ' ⬇️ Pega AQUÍ dentro TODAS las declaraciones que ya tienes ⬇️
 
-    '''<summary>
-    '''Control form1.
-    '''</summary>
-    '''<remarks>
-    '''Campo generado automáticamente.
-    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
-    '''</remarks>
-    Protected WithEvents form1 As Global.System.Web.UI.HtmlControls.HtmlForm
+        Protected WithEvents form1 As Global.System.Web.UI.HtmlControls.HtmlForm
 
-    '''<summary>
-    '''Control grdEUCs.
-    '''</summary>
-    '''<remarks>
-    '''Campo generado automáticamente.
-    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
-    '''</remarks>
-    Protected WithEvents grdEUCs As Global.System.Web.UI.WebControls.GridView
+        Protected WithEvents grdEUCs As Global.System.Web.UI.WebControls.GridView
+        Protected WithEvents btnAgregarEUC As Global.System.Web.UI.WebControls.Button
 
-    '''<summary>
-    '''Control btnAgregarEUC.
-    '''</summary>
-    '''<remarks>
-    '''Campo generado automáticamente.
-    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
-    '''</remarks>
-    Protected WithEvents btnAgregarEUC As Global.System.Web.UI.WebControls.Button
+        Protected WithEvents phMsgDoc As Global.System.Web.UI.WebControls.PlaceHolder
+        Protected WithEvents vsDoc As Global.System.Web.UI.WebControls.ValidationSummary
+        Protected WithEvents hfEUCID_Doc As Global.System.Web.UI.WebControls.HiddenField
+        Protected WithEvents txtNombreEUC As Global.System.Web.UI.WebControls.TextBox
+        Protected WithEvents rfvNombreEUC As Global.System.Web.UI.WebControls.RequiredFieldValidator
+        Protected WithEvents txtProposito As Global.System.Web.UI.WebControls.TextBox
+        Protected WithEvents rfvProposito As Global.System.Web.UI.WebControls.RequiredFieldValidator
+        Protected WithEvents txtProceso As Global.System.Web.UI.WebControls.TextBox
+        Protected WithEvents rfvProceso As Global.System.Web.UI.WebControls.RequiredFieldValidator
+        Protected WithEvents txtUso As Global.System.Web.UI.WebControls.TextBox
+        Protected WithEvents rfvUso As Global.System.Web.UI.WebControls.RequiredFieldValidator
+        Protected WithEvents txtInsumos As Global.System.Web.UI.WebControls.TextBox
+        Protected WithEvents rfvInsumos As Global.System.Web.UI.WebControls.RequiredFieldValidator
+        Protected WithEvents txtResponsableDoc As Global.System.Web.UI.WebControls.TextBox
+        Protected WithEvents rfvResponsableDoc As Global.System.Web.UI.WebControls.RequiredFieldValidator
+        Protected WithEvents txtDocTecnica As Global.System.Web.UI.WebControls.TextBox
+        Protected WithEvents txtEvControl As Global.System.Web.UI.WebControls.TextBox
+        Protected WithEvents btnGuardarDoc As Global.System.Web.UI.WebControls.Button
 
-    '''<summary>
-    '''Control hfEUCID_Doc.
-    '''</summary>
-    '''<remarks>
-    '''Campo generado automáticamente.
-    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
-    '''</remarks>
-    Protected WithEvents hfEUCID_Doc As Global.System.Web.UI.WebControls.HiddenField
+        Protected WithEvents phMsgPlan As Global.System.Web.UI.WebControls.PlaceHolder
+        Protected WithEvents vsPlan As Global.System.Web.UI.WebControls.ValidationSummary
+        Protected WithEvents hfEUCID_Plan As Global.System.Web.UI.WebControls.HiddenField
+        Protected WithEvents txtHerramientasPlan As Global.System.Web.UI.WebControls.TextBox
+        Protected WithEvents rfvHerr As Global.System.Web.UI.WebControls.RequiredFieldValidator
+        Protected WithEvents txtFechaEstimadaPlan As Global.System.Web.UI.WebControls.TextBox
+        Protected WithEvents rfvFecha As Global.System.Web.UI.WebControls.RequiredFieldValidator
+        Protected WithEvents cvFecha As Global.System.Web.UI.WebControls.CompareValidator
+        Protected WithEvents rvFechaMin As Global.System.Web.UI.WebControls.RangeValidator
+        Protected WithEvents txtResponsablePlan As Global.System.Web.UI.WebControls.TextBox
+        Protected WithEvents rfvResp As Global.System.Web.UI.WebControls.RequiredFieldValidator
+        Protected WithEvents txtPlan As Global.System.Web.UI.WebControls.TextBox
+        Protected WithEvents btnGuardarPlan As Global.System.Web.UI.WebControls.Button
 
-    '''<summary>
-    '''Control txtNombreEUC.
-    '''</summary>
-    '''<remarks>
-    '''Campo generado automáticamente.
-    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
-    '''</remarks>
-    Protected WithEvents txtNombreEUC As Global.System.Web.UI.WebControls.TextBox
+        ' ⬆️ Pega aquí dentro TODAS tus declaraciones ⬆️
 
-    '''<summary>
-    '''Control txtProceso.
-    '''</summary>
-    '''<remarks>
-    '''Campo generado automáticamente.
-    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
-    '''</remarks>
-    Protected WithEvents txtProceso As Global.System.Web.UI.WebControls.TextBox
-
-    '''<summary>
-    '''Control txtUso.
-    '''</summary>
-    '''<remarks>
-    '''Campo generado automáticamente.
-    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
-    '''</remarks>
-    Protected WithEvents txtUso As Global.System.Web.UI.WebControls.TextBox
-
-    '''<summary>
-    '''Control txtInsumos.
-    '''</summary>
-    '''<remarks>
-    '''Campo generado automáticamente.
-    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
-    '''</remarks>
-    Protected WithEvents txtInsumos As Global.System.Web.UI.WebControls.TextBox
-
-    '''<summary>
-    '''Control txtResponsableDoc.
-    '''</summary>
-    '''<remarks>
-    '''Campo generado automáticamente.
-    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
-    '''</remarks>
-    Protected WithEvents txtResponsableDoc As Global.System.Web.UI.WebControls.TextBox
-
-    '''<summary>
-    '''Control txtDocTecnica.
-    '''</summary>
-    '''<remarks>
-    '''Campo generado automáticamente.
-    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
-    '''</remarks>
-    Protected WithEvents txtDocTecnica As Global.System.Web.UI.WebControls.TextBox
-
-    '''<summary>
-    '''Control txtEvControl.
-    '''</summary>
-    '''<remarks>
-    '''Campo generado automáticamente.
-    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
-    '''</remarks>
-    Protected WithEvents txtEvControl As Global.System.Web.UI.WebControls.TextBox
-
-    '''<summary>
-    '''Control btnGuardarDoc.
-    '''</summary>
-    '''<remarks>
-    '''Campo generado automáticamente.
-    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
-    '''</remarks>
-    Protected WithEvents btnGuardarDoc As Global.System.Web.UI.WebControls.Button
-
-    '''<summary>
-    '''Control hfEUCID_Plan.
-    '''</summary>
-    '''<remarks>
-    '''Campo generado automáticamente.
-    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
-    '''</remarks>
-    Protected WithEvents hfEUCID_Plan As Global.System.Web.UI.WebControls.HiddenField
-
-    '''<summary>
-    '''Control txtResponsablePlan.
-    '''</summary>
-    '''<remarks>
-    '''Campo generado automáticamente.
-    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
-    '''</remarks>
-    Protected WithEvents txtResponsablePlan As Global.System.Web.UI.WebControls.TextBox
-
-    '''<summary>
-    '''Control txtPlan.
-    '''</summary>
-    '''<remarks>
-    '''Campo generado automáticamente.
-    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
-    '''</remarks>
-    Protected WithEvents txtPlan As Global.System.Web.UI.WebControls.TextBox
-
-    '''<summary>
-    '''Control btnGuardarPlan.
-    '''</summary>
-    '''<remarks>
-    '''Campo generado automáticamente.
-    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
-    '''</remarks>
-    Protected WithEvents btnGuardarPlan As Global.System.Web.UI.WebControls.Button
-End Class
+    End Class
+End Namespace
--- TDG/TDG/PoliticasEUC/DesarrolladorEUC.aspx.vb
+++ TDG/TDG/PoliticasEUC/DesarrolladorEUC.aspx.vb
@@ -1,10 +1,119 @@
 ﻿
+Imports System
+Imports System.Web.UI
+Imports System.Web.UI.WebControls
+
 Namespace TDG
     Partial Public Class DesarrolladorEUC
         Inherits System.Web.UI.Page
 
+        ' =====================================================================
+        '  Page_Load: fuerza validación clásica y configura el rango de fechas
+        ' =====================================================================
         Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
-            ' Tu lógica aquí
+            ' Habilita los validadores clásicos (si no usas unobtrusive scripts)
+            Me.UnobtrusiveValidationMode = System.Web.UI.UnobtrusiveValidationMode.None
+
+            If Not IsPostBack Then
+                ' Configurar rango mínimo/máximo para la fecha del plan (si el validador existe en el .aspx)
+                If rvFechaMin IsNot Nothing Then
+                    rvFechaMin.MinimumValue = DateTime.Today.ToString("yyyy-MM-dd")        ' hoy
+                    rvFechaMin.MaximumValue = DateTime.Today.AddYears(10).ToString("yyyy-MM-dd") ' +10 años
+                End If
+            End If
         End Sub
+
+        ' =====================================================================
+        '  GRID: Acciones por fila (abre modales)
+        '   - "Ver": por ahora lo usamos para abrir Documentación (ajústalo a gusto)
+        '   - "Doc": abre modal de Documentación
+        '   - "Plan": abre modal de Plan
+        ' =====================================================================
+        Protected Sub grdEUCs_RowCommand(sender As Object, e As GridViewCommandEventArgs)
+            If String.IsNullOrWhiteSpace(Convert.ToString(e.CommandArgument)) Then Exit Sub
+
+            Dim id As Integer
+            If Not Integer.TryParse(Convert.ToString(e.CommandArgument), id) Then Exit Sub
+
+            Select Case e.CommandName
+                Case "Ver", "Doc"
+                    hfEUCID_Doc.Value = id.ToString()
+                    ShowModal("#modalDocumentacion")
+
+                Case "Plan"
+                    hfEUCID_Plan.Value = id.ToString()
+                    ShowModal("#modalPlan")
+            End Select
+        End Sub
+
+        ' =====================================================================
+        '  Botón: Agregar nueva EUC -> abre Documentación
+        ' =====================================================================
+        Protected Sub btnAgregarEUC_Click(sender As Object, e As EventArgs)
+            hfEUCID_Doc.Value = "" ' nueva
+            ShowModal("#modalDocumentacion")
+        End Sub
+
+        ' =====================================================================
+        '  DOCUMENTACIÓN: Guardar (solo validación + mensaje)
+        ' =====================================================================
+        Protected Sub btnGuardarDoc_Click(sender As Object, e As EventArgs)
+            Page.Validate("DocForm")
+            If Not Page.IsValid Then
+                ShowDocAlert("Hay errores en el formulario. Por favor revísalos.", "danger")
+                ShowModal("#modalDocumentacion")
+                Exit Sub
+            End If
+
+            ' Aquí iría tu lógica real de guardado (no incluida hoy)
+            ShowDocAlert("Documentación guardada correctamente (simulado).", "success")
+            ShowModal("#modalDocumentacion")
+        End Sub
+
+        Private Sub ShowDocAlert(message As String, type As String)
+            ' type: success | info | warning | danger
+            phMsgDoc.Controls.Clear()
+            Dim html = $"<div class='alert alert-{type} alert-dismissible fade show' role='alert'>" &
+                       $"{Server.HtmlEncode(message)}" &
+                       "<button type='button' class='close' data-dismiss='alert' aria-label='Close'>&times;</button>" &
+                       "</div>"
+            phMsgDoc.Controls.Add(New LiteralControl(html))
+        End Sub
+
+        ' =====================================================================
+        '  PLAN DE AUTOMATIZACIÓN: Guardar (solo validación + mensaje)
+        ' =====================================================================
+        Protected Sub btnGuardarPlan_Click(sender As Object, e As EventArgs)
+            Page.Validate("PlanForm")
+            If Not Page.IsValid Then
+                ShowPlanAlert("Corrige los errores del formulario del Plan.", "danger")
+                ShowModal("#modalPlan")
+                Exit Sub
+            End If
+
+            ' Aquí iría tu lógica real de guardado (no incluida hoy)
+            ShowPlanAlert("Plan de Automatización guardado (simulado).", "success")
+            ShowModal("#modalPlan")
+        End Sub
+
+        Private Sub ShowPlanAlert(message As String, type As String)
+            phMsgPlan.Controls.Clear()
+            Dim html = $"<div class='alert alert-{type} alert-dismissible fade show' role='alert'>" &
+                       $"{Server.HtmlEncode(message)}" &
+                       "<button type='button' class='close' data-dismiss='alert' aria-label='Close'>&times;</button>" &
+                       "</div>"
+            phMsgPlan.Controls.Add(New LiteralControl(html))
+        End Sub
+
+        ' =====================================================================
+        '  UTIL: Reabrir modal de Bootstrap después del PostBack
+        '  Requiere jQuery + Bootstrap JS cargados en la página.
+        ' =====================================================================
+        Private Sub ShowModal(modalSelector As String)
+            Dim script As String = $"$(function() {{ $('{modalSelector}').modal('show'); }});"
+            ' Usamos ClientScript para no depender de <asp:ScriptManager>
+            ClientScript.RegisterStartupScript(Me.GetType(), Guid.NewGuid().ToString(), script, True)
+        End Sub
+
     End Class
 End Namespace
